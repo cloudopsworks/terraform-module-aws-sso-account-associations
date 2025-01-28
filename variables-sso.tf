91ed7d624cac58676ca3add7e35ce9d794e5a1d6
Cristian Beraha <cristian@cloudops.works>
2025-01-28 17:52:51 -0300

    Add YAML

--- variables-sso.tf
+++ variables-sso.tf
@@ -4,6 +4,12 @@
 #            Distributed Under Apache v2.0 License
 #
 
+## YAML settings
+# sso_associations:
+#   - permission_set: "AdministratorAccess"
+#     group_name: "Admins"
+#   - permission_set: "PowerUserAccess"
+#     group_name: "PowerUsers"
 variable "associations" {
   description = "List of permission set associations to create in the identity store"
   type        = any
