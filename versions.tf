0b3154dc604e4bd2ba891b21a71c76c75470f40f
Cristian Beraha <cristian@cloudops.works>
2025-07-23 13:23:26 -0300

    chore: Add Secrets Manager support, enhance variable definitions, and upgrade AWS provider version

--- versions.tf
+++ versions.tf
@@ -12,7 +12,8 @@ terraform {
   # Complete with required providers for the module
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.81"
     }
   }
 }
